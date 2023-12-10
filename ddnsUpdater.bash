2e9dea976bcf60ebd28007a91e7fb7a65961ca39
Lorenzo De Luca <32011163+lorenzodeluca@users.noreply.github.com>
2023-12-10 20:16:12 +0100

    Update ddnsUpdater.bash

--- ddnsUpdater.bash
+++ ddnsUpdater.bash
@@ -3,10 +3,10 @@
 USERNAME="UsernameYouCanGetFromYourRegistrar"
 PASSWORD="PasswordYouCanGetFromYourRegistrar"
 HOSTNAME="mydomain.it"
-REGISTRARLINK="ddns.registrar.example/nic/update"
+REGISTRARLINK="ddns.registrar.example"
 
 # Resolve current public IP
 IP=$(curl -s "https://domains.google.com/checkip")
 # Update DNS Record
-URL="https://${USERNAME}:${PASSWORD}@${REGISTRARLINK}hostname=${HOSTNAME}&myip=${IP}"
+URL="https://${USERNAME}:${PASSWORD}@${REGISTRARLINK}/nic/update?hostname=${HOSTNAME}&myip=${IP}"
 curl -s $URL
